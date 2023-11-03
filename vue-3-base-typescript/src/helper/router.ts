3a327c82e94f4064dc7aa396f7332d4e956ce1fd
hieplangsen <122256414+hieplangsen@users.noreply.github.com>
2023-11-03 09:03:54 +0000

    open router

--- vue-3-base-typescript/src/helper/router.ts
+++ vue-3-base-typescript/src/helper/router.ts
@@ -5,63 +5,63 @@ import { getAccessToken } from "@/composables/useLocalStorage";
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		// {
-		// 	path: "/manage",
-		// 	component: AppLayout,
-		// 	meta: {
-		// 		requiresAuth: true
-		// 	},
-		// 	children: [
-		// 		{
-		// 			path: "/dashboard",
-		// 			name: "dashboard",
-		// 			component: () => import("@/views/dashboard/Dashboard.vue")
-		// 		}
-		// 		// {
-		// 		// 	path: "/live/:id",
-		// 		// 	name: "live-detail",
-		// 		// 	meta: {
-		// 		// 		breadcrumb: ["Live stream"]
-		// 		// 	},
-		// 		// 	component: () => import("@/views/live/Detail.vue")
-		// 		// }
-		// 	]
-		// },
-		// {
-		// 	path: "/login",
-		// 	name: "login",
-		// 	component: () => import("@/views/auth/Login.vue")
-		// },
+		{
+			path: "/manage",
+			component: AppLayout,
+			meta: {
+				requiresAuth: true
+			},
+			children: [
+				{
+					path: "/dashboard",
+					name: "dashboard",
+					component: () => import("@/views/dashboard/Dashboard.vue")
+				}
+				// {
+				// 	path: "/live/:id",
+				// 	name: "live-detail",
+				// 	meta: {
+				// 		breadcrumb: ["Live stream"]
+				// 	},
+				// 	component: () => import("@/views/live/Detail.vue")
+				// }
+			]
+		},
+		{
+			path: "/login",
+			name: "login",
+			component: () => import("@/views/auth/Login.vue")
+		},
 		{
 			path: "/",
 			name: "landing page",
 			component: () => import("@/views/landing/LandingPage.vue")
 		},
-		// {
-		// 	path: "/home",
-		// 	name: "home not auth",
-		// 	component: () => import("@/views/layoutNotAuth/HomeNotAuth.vue")
-		// },
-		// {
-		// 	path: "/detail/:id",
-		// 	name: "detail not auth",
-		// 	component: () => import("@/views/layoutNotAuth/DetailNotAuth.vue")
-		// },
-		// {
-		// 	path: "/list-pending",
-		// 	name: "list-pending",
-		// 	component: () => import("@/views/layoutNotAuth/ListPending.vue")
-		// },
-		// {
-		// 	path: "/list-accept",
-		// 	name: "list-accept",
-		// 	component: () => import("@/views/layoutNotAuth/ListAccept.vue")
-		// },
-		// {
-		// 	path: "/list-reject",
-		// 	name: "list-reject",
-		// 	component: () => import("@/views/layoutNotAuth/ListReject.vue")
-		// }
+		{
+			path: "/home",
+			name: "home not auth",
+			component: () => import("@/views/layoutNotAuth/HomeNotAuth.vue")
+		},
+		{
+			path: "/detail/:id",
+			name: "detail not auth",
+			component: () => import("@/views/layoutNotAuth/DetailNotAuth.vue")
+		},
+		{
+			path: "/list-pending",
+			name: "list-pending",
+			component: () => import("@/views/layoutNotAuth/ListPending.vue")
+		},
+		{
+			path: "/list-accept",
+			name: "list-accept",
+			component: () => import("@/views/layoutNotAuth/ListAccept.vue")
+		},
+		{
+			path: "/list-reject",
+			name: "list-reject",
+			component: () => import("@/views/layoutNotAuth/ListReject.vue")
+		}
 	]
 });
 
